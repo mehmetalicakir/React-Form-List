ce9fe7860d1860e27e0eade5cbab5471f7e0c1b1
mehmetalicakir <cakiroglu_m.ali@hotmail.com>
2021-12-05 10:18:42 +0300

    try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import AddUser from ".components/AddUser.js"
+import AddUser from "./components/AddUser.js"
 import logo from "./logo.svg"
 import "./App.css"
 
